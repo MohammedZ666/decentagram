9bb032867dc4cc4a10f886ed864f5b9a3aaaa298
Abu Obaida Zishan <abuobaidazishan@pop-os.localdomain>
2021-12-10 11:38:51 +0600

    initial commit

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -1,5 +1,31 @@
-pragma solidity ^0.5.0;
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.5.16;
 
 contract Decentragram {
-  // Code goes here...
+    string public name = "Decentragram";
+
+    //Store image
+    uint256 public imageCount = 0;
+    mapping(uint256 => Image) public images;
+
+    //Image struct
+    struct Image {
+        uint256 id;
+        string hash;
+        string description;
+        uint256 tipAmount;
+        address payable author;
+    }
+
+    //Create image
+    function uploadImage(string memory _imgHash, string memory _description)
+        public
+    {
+        //Increament image count
+        imageCount = imageCount++;
+
+        //Add Imaget to contract
+        images[1] = Image(imageCount, _imgHash, _description, 0, msg.sender);
+    }
+    //Tip images
 }
